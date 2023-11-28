a57f8ab8d271a3f4352d7be4cdb91a0975908db9
Roman Mullier <romanmullier@microsoft.com>
2023-11-28 11:53:39 -0500

    added vector search

--- retrieve_chunks.py
+++ retrieve_chunks.py
@@ -2,22 +2,27 @@ from typing import List
 from promptflow import tool
 from azure.search.documents import SearchClient
 from azure.core.credentials import AzureKeyCredential
+from azure.search.documents.models import VectorizedQuery
 from promptflow.connections import CognitiveSearchConnection
 
 @tool
-def retrieve_documentation(question: str, index_name: str,  search: CognitiveSearchConnection) -> str:
+def retrieve_documentation(question: str, index_name: str, embedding: List[float], search: CognitiveSearchConnection, full_text_search: bool, vector_search: bool) -> str:
   
   search_client = SearchClient(endpoint=search.api_base, 
                                 index_name=index_name, 
                                 credential=AzureKeyCredential(search.api_key))
   
 
-  results = search_client.search(
-    search_text=question,
-    top=1,
-    search_fields=["chunk"],
-    
-  )
+  search_parameters = { "top" : 1, "search_fields" : ["chunk"]}
+
+  if full_text_search:
+    search_parameters["search_text"] = question
+  
+  if vector_search:
+    vector_query = VectorizedQuery(vector=embedding, fields="vector")
+    search_parameters["vector_queries"] = [vector_query]
+
+  results = search_client.search(**search_parameters)
 
   chunks = [result["chunk"] for result in results]
 
